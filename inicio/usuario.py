5de4b13b63811959e0328ae6999b4d4c82d9219e
JHostin <pereyrajhostin@gmail.com>
2026-05-27 09:54:49 -0400

    Add files via upload
    segunda actualización del proyecto

--- inicio/usuario.py
+++ inicio/usuario.py
@@ -1,21 +1,19 @@
 usuario_guardado = ""
 contraseña_guardada = ""
-
 def registrar_usuario(): #registro de usuario
     global usuario_guardado, contraseña_guardada
     print(" REGISTRO ")
     usuario_guardado = input("Crear usuario: ").strip()#usuario nuevo
     contraseña_guardada = input("Crear contraseña: ").strip()#contraseña nueva
     print("Usuario registrado correctamente.")
-
-def iniciar_sesion():#números de intentos
+def iniciar_sesion():#número de intentos
     intentos_maximos = 3
     intentos = 0  
     while intentos < intentos_maximos:
         print(f" INICIAR SESIÓN (Intento {intentos + 1}/{intentos_maximos}) ")
-        usuario = input("Usuario: ").strip()
-        contraseña = input("Contraseña: ").strip()      
-        if usuario == usuario_guardado and contraseña == contraseña_guardada:
+        usuario = input("Usuario: ").strip()#pedir usuario
+        contraseña = input("Contraseña: ").strip()#pedir contraseña      
+        if usuario == usuario_guardado and contraseña == contraseña_guardada:#continuar si ambas opciones se cumplen
             print("Acceso concedido.")#cerrar el maximo de intentos
             return True
         else:
@@ -26,14 +24,13 @@ def iniciar_sesion():#números de intentos
                 print(f"Te quedan {intentos_restantes} intentos.") #mostrar el número de intentos restantes          
     print("Acceso bloqueado. Has superado el límite de intentos.") #mostrar cuando ya no queden intentos
     return False
-
 def menu(): #pantalla del menú
      while True:
         print("""    INICIO 
         1. Iniciar sesión
         2. Salir """)
         
-        opcion = input("Seleccione una opción: ").strip().upper()
+        opcion = input("Seleccione una opción: ").strip().upper()#seleccion de opcion
         
         if opcion == "1":     
             if iniciar_sesion():
