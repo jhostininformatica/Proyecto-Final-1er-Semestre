eac3335b2f518fa6a259ed328c8e1df06548bd10
JHostin <pereyrajhostin@gmail.com>
2026-06-01 09:10:16 -0400

    PARTE DOS DEL PROYECTO

--- inicio/error404.py
+++ inicio/error404.py
@@ -1,18 +1,18 @@
 usuario_guardado = ""
 contraseña_guardada = ""
 def registrar_usuario():#registro de usuario
-    global usuario_guardado
-    global contraseña_guardada
+    global usuario_guardado#usuario guardado
+    global contraseña_guardada#contraseña guardada
     print(" REGISTRO ")
-    usuario_guardado = input("Crear usuario: ").strip()
-    contraseña_guardada = input("Crear contraseña: ").strip()
+    usuario_guardado = input("Crear usuario: ").strip()#guardar nuevo usuario
+    contraseña_guardada = input("Crear contraseña: ").strip()#guardar nueva contraseña
     print(" Usuario registrado correctamente")
 
 def iniciar_sesion():
     print(" INICIAR SESIÓN ")
-    usuario = input("Usuario: ").strip()
-    contraseña = input("Contraseña: ").strip()
-    if usuario == usuario_guardado and contraseña == contraseña_guardada:
+    usuario = input("Usuario: ").strip()#pedir usuario
+    contraseña = input("Contraseña: ").strip()#pedir contraseña
+    if usuario == usuario_guardado and contraseña == contraseña_guardada:#mostrar si ambos se cumplen
         print(" Acceso concedido")
         return True
     else:
@@ -20,40 +20,40 @@ def iniciar_sesion():
         return False
 peliculas_vistas = 0
 while True:
-    print(""" INICIO 
+    print("""  INICIO 
           
 1. Iniciar sesión
 2. Registrarse
 3. Salir""")
-    opcion = input("Seleccione una opción: ").strip()   
-    if opcion == "1":# Iniciar sesión
+    opcion = input("Seleccione una opción: ").strip()#seleccion de opciones   
+    if opcion == "1":#iniciar sesión
         if iniciar_sesion():
             while True:
-                print(""" LETTERBOXD
+                print("""   LETTERBOXD
                        
 1. Ver catálogo
 2. Ver cantidad de catálogos vistos
 3. Cerrar sesion""")
-                opcion_menu = input("Seleccione una opción: ").strip()               
-                if opcion_menu == "1":# Mostrar catálogo
+                opcion_menu = input("Seleccione una opción: ").strip()#seleccion de opciones del segundo panel               
+                if opcion_menu == "1":#mostrar catálogo de las peliculas
                     print(" CATÁLOGO ")
-                    print("1. Interstellar")
-                    print("2. Titanic")
-                    print("3. John Wick")
-                    print("4. Toy Story")
-                    print("5. The Conjuring")
+                    print("1. Eternal Sunshine of the Spotless Mind")
+                    print("2. All the Bright Places")
+                    print("3. Ruby Sparks")
+                    print("4. Scott Pilgrim vs. the World")
+                    print("5. The Girl Next Door")
                     peliculas_vistas += 1                
-                elif opcion_menu == "2":# Mostrar contador
+                elif opcion_menu == "2":#mostrar contador de peliculas vistas
                     print(f" Catálogos vistos: {peliculas_vistas}")                
-                elif opcion_menu == "3":# Cerrar sesión
+                elif opcion_menu == "3":#cerrar sesión
                     print(" Cerrando sesión...")
                     break
                 else:
                     print(" Opción inválida")   
-    elif opcion == "2":# Registrarse
+    elif opcion == "2":#registrarse
         registrar_usuario()
-    elif opcion == "3":#salir
-        print(" Cerrando sistema...")
-        break
+    elif opcion == "3" or opcion == "SALIR": #sale del sistema
+            print("Calabaza calabaza 🎃 cada quien para su casa ")
+            break
     else:
         print(" Opción inválida")
